4cbb3bb603b61f880c30e810a1d3d3eb73402138
Juan Esteban <juanestban000@gmail.com>
2021-10-13 16:08:18 -0500

    Feat: add the package and files that will need the project for use

---
+++ src/models/PackageNecessary.ts
@@ -0,0 +1,4 @@
+export const createNpmRc = 'echo "save-exact=true" > .npmrc';
+
+export const packageNecessary =
+  'npm i prop-types react-icons styled-components framer-motion react-router-dom react-helmet react-helmet-async classnames';
