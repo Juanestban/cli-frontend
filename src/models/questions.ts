ef37f1bce75870845cae9ce1ade291281d1004fe
Juan Esteban <juanestban000@gmail.com>
2021-10-13 16:13:57 -0500

    feat: file only questions inquirer

---
+++ src/models/questions.ts
@@ -0,0 +1,34 @@
+import { QuestionCollection } from 'inquirer';
+import { TypesStateSession } from '../interfaces/TypesTateSession';
+
+export const questions: QuestionCollection<any> = [
+  {
+    type: 'input',
+    name: TypesStateSession.nameApplication,
+    message: 'What is the name of the project?',
+  },
+  {
+    type: 'list',
+    name: TypesStateSession.nextOrReact,
+    message: 'what are the library/framework that you will use?',
+    choices: ['React', 'Next'],
+  },
+  {
+    type: 'confirm',
+    name: TypesStateSession.ifImplementTs,
+    message: 'are you wish implement typescript?',
+    default: false,
+  },
+  {
+    type: 'confirm',
+    name: TypesStateSession.cleanners,
+    message: 'are you install eslint, prettier and lintstaged?',
+    default: true,
+  },
+  {
+    type: 'list',
+    name: TypesStateSession.typeOf_SO,
+    message: 'what are you system operative?',
+    choices: ['Windows', 'Mac', 'Linux'],
+  },
+];
