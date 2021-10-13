3b53cf184aa02590e4ae7aa17f7ad4016bf10317
Juan Esteban <juanestban000@gmail.com>
2021-10-13 16:13:13 -0500

    refactor file

--- src/models/PackageNecessary.ts
+++ src/models/PackageNecessary.ts
@@ -2,3 +2,7 @@ export const createNpmRc = 'echo "save-exact=true" > .npmrc';
 
 export const packageNecessary =
   'npm i prop-types react-icons styled-components framer-motion react-router-dom react-helmet react-helmet-async classnames';
+
+const packageNecessaryDefault: string[] = [createNpmRc, packageNecessary];
+
+export default packageNecessaryDefault;
