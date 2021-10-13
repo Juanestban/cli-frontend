3a92e9c54c38cdddec445734bbccdd0992186a36
Juan Esteban <juanestban000@gmail.com>
2021-10-13 16:12:46 -0500

    Feat: add interfaces

--- src/interfaces/TypesTateSession.ts
+++ src/interfaces/TypesTateSession.ts
@@ -1,6 +1,19 @@
+/*
+export interface Questions {
+  nextOrReact: 'React' | 'Next';
+  ifImplementTs: boolean;
+  cleanners: boolean;
+  nameApplication: string;
+  typeOf_SO: 'Windows' | 'Mac' | 'Linux';
+}
+
+[path_File: __dirname/interfaces/Questions.ts]
+*/
+
 export enum TypesStateSession {
-  'nextOrReact',
-  'ifImplementTs',
-  'cleanners',
-  'typeOf_SO',
+  nameApplication = 'nameApplication',
+  nextOrReact = 'nextOrReact',
+  ifImplementTs = 'ifImplementTs',
+  cleanners = 'cleanners',
+  typeOf_SO = 'typeOf_SO',
 }
