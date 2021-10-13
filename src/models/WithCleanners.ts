30016e8055466969773124c6c74446679e3f9d0a
Juan Esteban <juanestban000@gmail.com>
2021-10-13 16:05:13 -0500

    Feat: add the commands and files that will need the project for have the cleaner code

---
+++ src/models/WithCleanners.ts
@@ -0,0 +1,59 @@
+const packageCleaners = [
+  'eslint-config-standard',
+  'eslint-plugin-import',
+  'eslint-plugin-jest',
+  'eslint-plugin-node',
+  'eslint-plugin-promise',
+  'eslint-plugin-react',
+  'eslint-plugin-testing-library',
+];
+
+export const installCleanners = `npm i eslint eslint-config-prettier prettier ${packageCleaners.join(
+  ' '
+)}`;
+
+const ignore = `.next
+node_modules
+.eslintcache
+.vscode
+coverage
+babel-config.js
+package.json
+.eslintcache
+`;
+
+export const prettier = `echo '{
+  "semi": false;
+  "singleQuote": true;
+  "tabWidth": 2;
+}' > .pretterrc`;
+
+export const prettierignore = `echo "${ignore}" > .prettierignore`;
+
+export const eslint = `echo "module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    'jest/globals': true,
+  },
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'testing-library', 'jest'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    camelcase: 0,
+    // 'no-debugger': 0,
+    'dot-notation': 0,
+  },
+}" > .eslintrc.js`;
+
+export const eslintignore = `echo "${ignore}" > .eslintignore`;
+
+export const lintStaged = 'npx mrm@2 lint-staged';
