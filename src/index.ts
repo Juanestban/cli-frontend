e6829aa4999535f668abb75d87d6fd054fae3286
Juan Esteban <juanestban000@gmail.com>
2021-10-13 09:07:29 -0500

    chore: add finish issue. Close #2

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import Interface from './models/Interface';
 // future references
 // import fs from 'fs'
 // fs.writeFile('index.js', 'console.log(\'index!!\')', {}, () => {})
+// another commit
 
 const interfa = new Interface();
 
