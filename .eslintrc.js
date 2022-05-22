1c01421838bee1a73eb24e7c35210e1177fde294
Juanestban <juanestban000@gmail.com>
2022-05-22 12:29:34 -0500

    chore: prettier, eslint, lint-staged and semantic release configurated

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'jsx-a11y/aria-role': 0,
+  },
+};
